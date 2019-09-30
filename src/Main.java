cc31793bbeb09d46530987778d2fb65dcd5c82f5
Nicki Hvolgaard <38239769+TheMagicalKing@users.noreply.github.com>
2019-09-30 11:07:47 +0200

    Angivet hand for to spillere
    Lavet funktion så RPS virker

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,9 @@ public class Main {
 
     public static void main(String[] args) {
         Hand nicki = Hand.PAPER;
+        Hand mik = Hand.ROCK;
+        RPS round1 = new RPS();
+        System.out.println(round1.battle(mik, nicki));
     }
 
 }
--- src/RPS.java
+++ src/RPS.java
@@ -1,2 +1,29 @@
 public class RPS {
+
+    int battle(Hand a, Hand b){
+        int resultat = -1;
+        if ( a == Hand.PAPER){
+            if (b == Hand.PAPER){
+                resultat = 0;
+            }
+            if (b == Hand.ROCK){
+                resultat = 1;
+            }
+            if (b == Hand.SCISSOR){
+                resultat = 2;
+            }
+        }
+        if (a == Hand.PAPER && b == Hand.ROCK) return 1;
+        else if (a == Hand.PAPER && b == Hand.SCISSOR) return 2;
+        else if (a == Hand.PAPER && b == Hand.PAPER) return 0;
+        else if (a == Hand.ROCK && b == Hand.SCISSOR) return 1;
+        else if (a == Hand.ROCK && b == Hand.PAPER) return 2;
+        else if (a == Hand.ROCK && b == Hand.ROCK) return 0;
+        else if (a == Hand.SCISSOR && b == Hand.PAPER) return 1;
+        else if (a == Hand.SCISSOR && b == Hand.ROCK) return 2;
+        else if (a == Hand.SCISSOR && b == Hand.SCISSOR) return 0;
+        else return -1;
+    }
+
+    //todo multiBattle(){ }
 }
