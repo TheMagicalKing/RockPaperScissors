f0509a0e58df562e5f70b72c53b1252ae075fce2
Nicki Hvolgaard <38239769+TheMagicalKing@users.noreply.github.com>
2019-09-30 10:36:52 +0200

    Forsøgt test, blevet introduceret til ENUM

--- Test/RPSTest.java
+++ Test/RPSTest.java
@@ -1,5 +1,18 @@
-import junit.framework.TestCase;
+/**import junit.framework.TestCase;
 
 public class RPSTest extends TestCase {
 
-}
+    //test om Spiller har vundet eller ej
+    public boolean testRPSPlayerWin(){
+        boolean win = false;
+        //input = Rock
+        //forventet output = loss
+        if (playInput == 0 && comInput == 1) {
+            return true;
+        }else
+            return win;
+
+    }
+
+
+}**/
---
+++ src/Hand.java
@@ -0,0 +1,5 @@
+public enum Hand {
+    ROCK,
+    PAPER,
+    SCISSOR
+}
