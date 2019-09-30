35505719af8c6f93860fb13885bcef3b7028a721
Nicki Hvolgaard <38239769+TheMagicalKing@users.noreply.github.com>
2019-09-30 11:51:01 +0200

    Gjort funktionalitet og bruger venlighed bedre

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,33 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        Hand nicki = Hand.PAPER;
-        Hand mik = Hand.ROCK;
-        RPS round1 = new RPS();
-        System.out.println(round1.battle(mik, nicki));
+        RPS playRPS = new RPS();
+        Hand comHand = null;
+        int handInt = (int) (Math.random()*3);
+        if (handInt == 0)comHand = Hand.ROCK;
+        if (handInt == 1)comHand = Hand.PAPER;
+        if (handInt == 2)comHand = Hand.SCISSOR;
+        Scanner input = new Scanner(System.in);
+        //System.out.println("How many times would you like to play?");
+        //int turnAmount = input.nextInt();
+
+        Hand playHand = null;
+        System.out.println("Please input your Rock, Paper, Scissor choice");
+        String playString = input.next();
+        if (playString.equalsIgnoreCase("rock"))playHand = Hand.ROCK;
+        if (playString.equalsIgnoreCase("paper"))playHand = Hand.PAPER;
+        if (playString.equalsIgnoreCase("scissor"))playHand = Hand.SCISSOR;
+        //playRPS.multiBattle(playHand,comHand,turnAmount);
+        System.out.println("Computer's hand is: " + comHand);
+        System.out.println(playRPS.battle(playHand,comHand));
+        int resultat = playRPS.battle(playHand,comHand);
+
+        if (resultat == 0) System.out.println("Computer played the same hand as you");
+        if (resultat == 1) System.out.println("Computer played the wrong hand you win");
+        if (resultat == 2) System.out.println("Computer played the right hand Computer wins");
+        while
     }
 
 }
--- src/RPS.java
+++ src/RPS.java
@@ -2,7 +2,7 @@ public class RPS {
 
     int battle(Hand a, Hand b){
         int resultat = -1;
-        if ( a == Hand.PAPER){
+        /**if ( a == Hand.PAPER){
             if (b == Hand.PAPER){
                 resultat = 0;
             }
@@ -12,7 +12,7 @@ public class RPS {
             if (b == Hand.SCISSOR){
                 resultat = 2;
             }
-        }
+        }**/
         if (a == Hand.PAPER && b == Hand.ROCK) return 1;
         else if (a == Hand.PAPER && b == Hand.SCISSOR) return 2;
         else if (a == Hand.PAPER && b == Hand.PAPER) return 0;
@@ -25,5 +25,22 @@ public class RPS {
         else return -1;
     }
 
-    //todo multiBattle(){ }
+   /** int  multiBattle(Hand a, Hand b,int turnAmount){
+        int count = 0;
+        int resultat=-1;
+        while (count <= turnAmount){
+            if (a == Hand.PAPER && b == Hand.ROCK) return 1;
+            else if (a == Hand.PAPER && b == Hand.SCISSOR) return 2;
+            else if (a == Hand.PAPER && b == Hand.PAPER) return 0;
+            else if (a == Hand.ROCK && b == Hand.SCISSOR) return 1;
+            else if (a == Hand.ROCK && b == Hand.PAPER) return 2;
+            else if (a == Hand.ROCK && b == Hand.ROCK) return 0;
+            else if (a == Hand.SCISSOR && b == Hand.PAPER) return 1;
+            else if (a == Hand.SCISSOR && b == Hand.ROCK) return 2;
+            else if (a == Hand.SCISSOR && b == Hand.SCISSOR) return 0;
+            else return resultat;
+            count++;
+        }
+
+    }*/
 }
